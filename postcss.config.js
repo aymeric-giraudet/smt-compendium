ef147374c902d4338b2c69db69a394e7d0527f2f
Aymeric Giraudet <neadj59@gmail.com>
2020-05-15 15:45:50 +0200

    set CSS purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,16 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./shared/**/*.tsx", "./pages/**/*.tsx", "./components/**/*.tsx"],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    whitelist: ["body"]
+  },
+];
+
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env'],
-}
+  plugins: [
+    "tailwindcss",
+    "postcss-preset-env",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
